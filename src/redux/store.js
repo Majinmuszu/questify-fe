7e5707833d6a21bf3a4dc0d96c1e80f662202668
majinmuszu <artur.krasniewski1@gmail.com>
2022-04-26 03:05:36 +0200

    added header RWD and logic for logout
    also added fonts (pay attention to font-family name!)
    added currentUser in store
    added conditional header rendering

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { questifyApi } from "../services/api";
-import { tokenReducer } from "./reducers";
+import { tokenReducer, userReducer } from "./reducers";
 
 const preloadedState = {
   authToken: null,
+  currentUser: { email: "test@mail.com" },
 };
 export const store = configureStore({
   reducer: {
     [questifyApi.reducerPath]: questifyApi.reducer,
     authToken: tokenReducer,
+    currentUser: userReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(questifyApi.middleware),
